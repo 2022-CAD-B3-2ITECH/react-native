dc62d6f1755afa6178d532b275b6d18cbe887f8d
gganster <guilian.ganster@wisoftify.fr>
2022-11-23 09:18:16 +0100

    exemple hook api

---
+++ rn/hooks/useMeteo.js
@@ -0,0 +1,22 @@
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+
+const useMeteo = () => {
+  //useState
+
+  useEffect(() => {
+    async function fetchData() {
+      //call api
+      await axios.get("/api/meteo")
+    }
+    fetchData();
+  }, [])
+
+  useEffect(() => {
+    (async () => {
+      //call api
+    })()
+  })
+}
+
+export default useMeteo;
